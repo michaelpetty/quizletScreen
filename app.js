bee77ad7a484df95286f1c32a573cb226a5687c2
mpetty <michael.petty@gmail.com>
2020-01-23 13:07:32 -0800

    limiting recursion to smaller test sample

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const getDOMFromURL = async (url) => {
     return parser.parseFromString(body, 'text/html');
 }
 
-const getElementsFromClassNew = (ele, className) => {
+const getElementsFromClassNew = (ele, className, instNum = 1) => {
+  if (instNum > 2000) return;
   let match = [];
   //console.log(ele.children[0].children[1].children[1].children[3].children[2].classList);
-  let list = ele.classList
+  let list = ele.classList;
   for (i=0; i < list.length; i++) {
     console.log(list[i]);
     if (list[i] === className) match.push(ele);
   }
   let children = ele.children;
   for (i=0; i < children.length; i++) {
-    //console.log(children[i])
-    //match.push(getElementsFromClassNew(children[i], className));
+    console.log(children[i])
+    match.push(getElementsFromClassNew(children[i], className, instNum++));
   }
   return match;
 }
@@ -24,5 +25,6 @@ const getElementsFromClassNew = (ele, className) => {
 getDOMFromURL('https://developer.mozilla.org/en-US/docs/Web/API/Element')
 .then(res => {
   document.getElementById('builtIn').innerText = res.getElementsByClassName('external').length;
-  document.getElementById('homemade').innerText = getElementsFromClassNew(res.children[0], 'external').length;
+  document.getElementById('homemade').innerText = getElementsFromClassNew(res.children[0].children[1].children[1].children[3].children[2].children[2], 'toggle').length;
+  console.log('finished');
 })
