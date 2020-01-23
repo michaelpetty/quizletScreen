1a80d35b15d937adda467f6082af4b1c07a1d9dc
mpetty <michael.petty@gmail.com>
2020-01-23 09:14:18 -0800

    manual view of children and multiple classList

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const getDOMFromURL = async (url) => {
     const res = await fetch(url, {method: 'get'});
     const body = await res.text();
-    console.log(body);
     const parser = new DOMParser();
     return parser.parseFromString(body, 'text/html');
 }
 
 const getElementsFromClassNew = (ele, className) => {
-  console.log(ele);
+  console.log(ele.children[0].children[1].children[1].children[3].children[2].classList);
   return [];
 }
 
 getDOMFromURL('https://developer.mozilla.org/en-US/docs/Web/API/Element')
 .then(res => {
-  console.log(res.getElementsByClassName('external'));
+  console.log(res.getElementsByClassName('external').length);
   console.log(getElementsFromClassNew(res, 'external'));
 })
