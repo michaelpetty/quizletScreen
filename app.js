4412896f85867bb68b8e3249fb0534a6e42dfee5
mpetty <michael.petty@gmail.com>
2020-01-23 19:40:20 -0800

    clean up; remove limiter; set class to external

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const getDOMFromURL = async (url) => {
 }
 
 const getElementsFromClassNew = (ele, className, instNum = 1) => {
-  if (instNum > 200) return [];
+  //if (instNum > 200) return [];
   let match = [];
   //console.log(ele)
   //console.log(ele.children[0].children[1].children[1].children[3].children[2].classList);
   let list = ele.classList;
   for (let i=0; i < list.length; i++) {
-    console.log(instNum, list[i]);
+    //console.log(instNum, list[i]);
     if (list[i] === className) match.push(ele);
   }
   let children = ele.children;
-  console.log('children: ', children.length)
+  //console.log('children: ', children.length)
   for (let i=0; i < children.length; i++) {
-    console.log(instNum, children[i])
+    //console.log(instNum, children[i])
     match.push(getElementsFromClassNew(children[i], className, ++instNum));
   }
   return match.flat();
@@ -28,9 +28,10 @@ getDOMFromURL('https://developer.mozilla.org/en-US/docs/Web/API/Element')
 .then(res => {
   document.getElementById('builtIn').innerText = res.getElementsByClassName('external').length;
 
-  let homemade = getElementsFromClassNew(res.children[0].children[1].children[1].children[3].children[2].children[2].children[0].children[1].children[0], 'toggle');
+  let homemade = getElementsFromClassNew(res.children[0].children[1], 'external');
   console.log(homemade);
   document.getElementById('homemade').innerText = homemade.length;
+  //res.children[0].children[1].children[1].children[3].children[2].children[2].children[0].children[1].children[0].children[2]
   //res.children[0].children[1]
   console.log('finished');
 })
