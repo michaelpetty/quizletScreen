b5618cd17ac36ad788cbb85d67f6ff42a5f6b6f0
mpetty <michael.petty@gmail.com>
2020-01-23 19:32:07 -0800

    use let i in for loops; flatten result array

--- app.js
+++ app.js
@@ -6,25 +6,31 @@ const getDOMFromURL = async (url) => {
 }
 
 const getElementsFromClassNew = (ele, className, instNum = 1) => {
-  if (instNum > 2000) return;
+  if (instNum > 200) return [];
   let match = [];
+  //console.log(ele)
   //console.log(ele.children[0].children[1].children[1].children[3].children[2].classList);
   let list = ele.classList;
-  for (i=0; i < list.length; i++) {
-    console.log(list[i]);
+  for (let i=0; i < list.length; i++) {
+    console.log(instNum, list[i]);
     if (list[i] === className) match.push(ele);
   }
   let children = ele.children;
-  for (i=0; i < children.length; i++) {
-    console.log(children[i])
-    match.push(getElementsFromClassNew(children[i], className, instNum++));
+  console.log('children: ', children.length)
+  for (let i=0; i < children.length; i++) {
+    console.log(instNum, children[i])
+    match.push(getElementsFromClassNew(children[i], className, ++instNum));
   }
-  return match;
+  return match.flat();
 }
 
 getDOMFromURL('https://developer.mozilla.org/en-US/docs/Web/API/Element')
 .then(res => {
   document.getElementById('builtIn').innerText = res.getElementsByClassName('external').length;
-  document.getElementById('homemade').innerText = getElementsFromClassNew(res.children[0].children[1].children[1].children[3].children[2].children[2], 'toggle').length;
+
+  let homemade = getElementsFromClassNew(res.children[0].children[1].children[1].children[3].children[2].children[2].children[0].children[1].children[0], 'toggle');
+  console.log(homemade);
+  document.getElementById('homemade').innerText = homemade.length;
+  //res.children[0].children[1]
   console.log('finished');
 })
